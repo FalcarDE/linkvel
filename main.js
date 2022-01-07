c82251a55c4fa77b838fa6021ad384e062f4021e
du8915ng <duc.nguyen@fh-erfurt.de>
2022-01-07 18:09:25 +0100

    version_1

--- main.js
+++ main.js
@@ -46,3 +46,11 @@ accordionItemhHeader.forEach(accordionItemhHeader =>
 
 
 
+// Navigation ----------------------------------------------------------------------------------------------------------
+
+const toggleButton = document.getElementsByClassName("toggle-button")[0]
+const listContainer = document.getElementsByClassName("list-container")[0]
+
+toggleButton.addEventListener("click", () =>{
+    listContainer.classList.toggle("active")
+})
